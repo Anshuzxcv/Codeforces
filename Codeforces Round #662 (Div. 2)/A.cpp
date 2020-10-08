90f9854ba0f888737538155d972f169ef6a272ec
Anshuzxcv <anshuzxcv@gmail.com>
2020-10-08 23:44:43 +0530

    Adding Codeforces Round #662A

---
+++ Codeforces Round #662 (Div. 2)/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll T, n;
+    cin >> T;
+    while (T--)
+    {
+        cin >> n;
+        cout << n / 2 + 1 << endl;
+    }
+}
